319f2e6e2c35c537642155ea8013cfae0b3f8fef
Bekewei Egbekun <egbekunbikey@gmail.com>
2022-10-13 15:06:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,23 @@
-/*
- *  * File: 4-puts.c
- *    */
-
 #include <stdio.h>
 
+
+
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
