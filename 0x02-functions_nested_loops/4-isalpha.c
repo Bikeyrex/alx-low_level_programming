40b6d69e6309ada3f86ae782eb43470911219434
Bekewei Egbekun <egbekunbikey@gmail.com>
2022-10-18 15:58:50 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 
 /**
-* _isalpha 
+* _isalpha - ALX project
 * @c: An input character
 * Description: function returns 1 if the character is a
 * letter, lowercase or uppercase.
@@ -12,27 +12,17 @@
 int _isalpha(int c)
 
 {
-
 		char lower, upper;
 
 		int isletter = 0;
 
-
-
 		for (lower = 'a'; lower <= 'z'; lower++)
-
 		{
-
 			for (upper = 'A'; upper <= 'Z'; upper++)
-
 			{
-
 				if (c == lower || c == upper)
-
 					isletter = 1;
-
 			}
-
 		}
 
 		return (isletter);
