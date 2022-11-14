f9afea37d46a4c18a7daf05e6050b96f01130f5d
Bekewei Egbekun <egbekunbikey@gmail.com>
2022-11-14 14:13:41 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
-
-
-
-/*
-* File: dog.h
-* Auth: Bekewei G Egbekun
-* Desc: Header file that defines a new type struct dog.
-*/
+#ifndef _DOG_H_
+#define _DOG_H_
 
 
 /**
-* struct dog - A new type describing a dog.
-* @name: The name of the dog.
-* @age: The age of the dog.
-* @owner: The owner of the dog.
+* struct dog - structure for a basic dog
+* @name: Dog's name
+* @age: Dog's age
+* @owner: Dog's owner
+* Description: data structure for dog
 */
 
 struct dog
@@ -27,12 +20,7 @@ struct dog
 
 };
 
-/**
-* dog_t - Typedef for struct dog
-*/
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
